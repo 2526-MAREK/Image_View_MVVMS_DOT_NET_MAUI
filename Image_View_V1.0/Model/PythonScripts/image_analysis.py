beb827bc840772a08330700dda9781f1cd8ff18a
pananananas <101266606+pananananas@users.noreply.github.com>
2023-04-24 23:12:59 +0200

    KURWAAAAAAA DZIAŁA HISTOGRAM Z CHUNKU hIST!!!!!!!!
    ale zajebiście mordo, jeśli to czytasz to stawiam Ci piwko hahahah

--- Image_View_V1.0/Model/PythonScripts/image_analysis.py
+++ Image_View_V1.0/Model/PythonScripts/image_analysis.py
@@ -30,37 +30,54 @@ class ImageAnalysis:
         cv2.imwrite(output_path + 'fft.png', magnitude_spectrum)  # Save the magnitude spectrum to a file
 
     @staticmethod
+    def transform_hist(hist_data):
+        hist = hist_data['Histogram']
+        result = np.zeros((256, 1), dtype=float)
+
+        for i in range(len(hist)):
+            result[i] = float(hist[i])
+
+        return result
+
+    @staticmethod
     def histogram_of_image(image_path, output_folder, output_imgs_folder, draw_plots, hist_rgb, hIST):
         # Load the image
         img = cv2.imread(image_path)
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        # Compute histograms
-        # if hIST is None:
-        hist_gray = cv2.calcHist([img_gray], [0], None, [256], [0, 256])
-        # else:
-        # hist_gray = hIST
+        # Plot setup
+        fig = plt.figure(figsize=(10, 4))
+        ax = plt.axes()
+        ax.set_xlim([0, 256])
+        fig.set_facecolor((0.09375, 0.09375, 0.09375))
+        ax.set_facecolor((0.2, 0.2, 0.2))
+        frame = plt.gca()  # Remove the labels:
+        frame.axes.xaxis.set_ticklabels([])
+        frame.axes.yaxis.set_ticklabels([])
+
+        x_vals = np.arange(0, 256, 1)
+
+        bar_alpha = 0.5
+        bar_width = 1
+
         if hist_rgb:
             hist_r = cv2.calcHist([img_rgb], [0], None, [256], [0, 256])
             hist_g = cv2.calcHist([img_rgb], [1], None, [256], [0, 256])
             hist_b = cv2.calcHist([img_rgb], [2], None, [256], [0, 256])
+            ax.set_ylim([0, max(max(hist_r), max(hist_g), max(hist_b)) * 1.1])
+            plt.bar(x_vals, hist_r[:, 0], width=bar_width, color='r', alpha=bar_alpha)
+            plt.bar(x_vals, hist_g[:, 0], width=bar_width, color='g', alpha=bar_alpha)
+            plt.bar(x_vals, hist_b[:, 0], width=bar_width, color='b', alpha=bar_alpha)
 
+        else:
+            if hIST is None:
+                hist_gray = cv2.calcHist([img_gray], [0], None, [256], [0, 256])
+            else:
+                hist_gray = ImageAnalysis.transform_hist(hIST)
+            ax.set_ylim([0, max(hist_gray) * 1.1])
+            plt.bar(x_vals, hist_gray[:, 0], width=bar_width, color='w', alpha=bar_alpha)
 
-        fig = plt.figure(figsize=(10, 4))
-        ax = plt.axes()
-        fig.set_facecolor((0.09375, 0.09375, 0.09375))
-        ax.set_facecolor((0.2, 0.2, 0.2))
-
-        frame1 = plt.gca()  # Remove the labels:
-        frame1.axes.xaxis.set_ticklabels([])
-        frame1.axes.yaxis.set_ticklabels([])
-
-        plt.plot(hist_gray, color='w')
-        if hist_rgb:
-            plt.plot(hist_r, color='r')
-            plt.plot(hist_g, color='g')
-            plt.plot(hist_b, color='b')
         plt.tight_layout()
         plt.savefig(output_imgs_folder + 'hist.png')
         if draw_plots:
