7bcbd8d28addd6148c11e152c15f22278a8a2017
Marek Szulak <73235295+2526-MAREK@users.noreply.github.com>
2023-04-24 22:36:38 +0200

    add_image_helpers

---
+++ Image_View_V1.0/Helpers/ImageToProcessHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image_View_V1._0.Helpers
+{
+    public class ImageToProcessHelper
+    {
+        public async Task<byte[]> ImageSourceToByteArrayAsync(ImageSource imageSource)
+        {
+            StreamImageSource streamImageSource = (StreamImageSource)imageSource;
+            Stream stream = await streamImageSource.Stream(CancellationToken.None);
+            MemoryStream memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream);
+            return memoryStream.ToArray();
+        }
+
+        public ImageSource ByteArrayToImageSource(byte[] imageBytes)
+        {
+            if (imageBytes == null || imageBytes.Length == 0)
+            {
+                return null;
+            }
+            return ImageSource.FromStream(() => new MemoryStream(imageBytes));
+        }
+
+        public string SerializeChunkIHDR(ChunkIHDR chunkIHDR)
+        {
+            return JsonConvert.SerializeObject(chunkIHDR);
+        }
+
+        public ChunkIHDR DeserializeChunkIHDR(string json)
+        {
+            return JsonConvert.DeserializeObject<ChunkIHDR>(json);
+        }
+    }
+}
